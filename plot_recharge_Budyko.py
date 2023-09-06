6d091e4112679b8cf0be3653d48b66a5d80903ce
Sebastian Gnann <gnann@ad.umwelt.uni-potsdam.de>
2023-09-06 11:06:58 +0200

    clean up code

---
+++ plot_recharge_Budyko.py
@@ -0,0 +1,191 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from functions import get_nearest_neighbour, plotting_fcts
+import geopandas as gpd
+import xarray as xr
+import rasterio as rio
+
+# This script loads and analyses different datasets in Budyko space.
+
+def Budyko_curve(aridity, **kwargs):
+    # Budyko, M.I., Miller, D.H. and Miller, D.H., 1974. Climate and life (Vol. 508). New York: Academic press.
+    return np.sqrt(aridity * np.tanh(1 / aridity) * (1 - np.exp(-aridity)));
+
+def Berghuijs_recharge_curve(aridity):
+    alpha = 0.72
+    beta = 15.11
+    RR = alpha*(1-(np.log(aridity**beta+1)/(1+np.log(aridity**beta+1))))
+    return RR
+
+# check if folders exist
+results_path = "results/"
+if not os.path.isdir(results_path):
+    os.makedirs(results_path)
+
+figures_path = "figures/"
+if not os.path.isdir(figures_path):
+    os.makedirs(figures_path)
+
+# todo: plot Miguez Macho in absolute terms and for different continents
+
+# load data
+
+# todo: load netrad data
+# todo: load caravan again and use online signatures
+# todo:only use certain catchments, e.g. no human influences, long enough complete time series, etc.
+# Caravan
+df = pd.read_csv("./results/caravan_processed.csv")
+df_Caravan = df
+print("Finished Caravan.")
+
+"""
+# CAMELS
+data_path = "C:/Users/gnann/Documents/MATLAB/CAMELS_Matlab/"
+df = pd.read_csv(data_path + "CAMELS_table.csv")
+df_CAMELS = df
+print("Finished CAMELS.")
+"""
+
+# ERA5
+ds_ERA5 = xr.open_dataset(results_path + "ERA5_aggregated.nc4") # used to extract aridity for recharge datasets
+df_ERA5 = pd.read_csv(results_path + "ERA5_aggregated.csv")
+df_ERA5 = df_ERA5.sample(100000) # to reduce size
+print("Finished ERA5.")
+
+# Moeck
+data_path = "C:/Users/gnann/Documents/PYTHON/GHM_Comparison/"
+df = pd.read_csv(data_path + "data/global_groundwater_recharge_moeck-et-al.csv", sep=',')
+selected_data = []
+for lat, lon in zip(df['Latitude'], df['Longitude']):
+    data_point = ds_ERA5.sel(latitude=lat, longitude=lon, method='nearest')#['tp']#.values()
+    data_point["recharge"] = \
+        df.loc[np.logical_and(df["Latitude"] == lat, df["Longitude"]==lon)]["Groundwater recharge [mm/y]"].values[0]
+    selected_data.append(data_point)
+ds_combined = xr.concat(selected_data, dim='time')
+df_Moeck = ds_combined.to_dataframe()
+df_Moeck["recharge_ratio"] = df_Moeck["recharge"]/df_Moeck["tp"]
+print("Finished Moeck.")
+
+# MacDonald
+data_path = "C:/Users/gnann/Documents/PYTHON/GHM_Comparison/"
+df = pd.read_csv(data_path + "data/Recharge_data_Africa_BGS.csv", sep=';')
+selected_data = []
+for lat, lon in zip(df['Lat'], df['Long']):
+    data_point = ds_ERA5.sel(latitude=lat, longitude=lon, method='nearest')#['tp']#.values()
+    data_point["recharge"] = \
+        df.loc[np.logical_and(df["Lat"] == lat, df["Long"]==lon)]["Recharge_mmpa"].values[0]
+    selected_data.append(data_point)
+ds_combined = xr.concat(selected_data, dim='time')
+df_MacDonald = ds_combined.to_dataframe()
+df_MacDonald["recharge_ratio"] = df_MacDonald["recharge"]/df_MacDonald["tp"]
+print("Finished MacDonald.")
+
+"""
+# FLUXCOM
+data_path = "D:/Data/FLUXCOM/RS/ensemble/720_360/monthly/"
+name_list = ["H", "LE", "Rn"]
+var_list = ["H.RS.EBC-ALL.MLM-ALL.METEO-NONE.720_360.monthly.",
+            "LE.RS.EBC-ALL.MLM-ALL.METEO-NONE.720_360.monthly.",
+            "Rn.RS.EBC-NONE.MLM-ALL.METEO-NONE.720_360.monthly."]
+years = ["2001", "2002", "2003", "2004", "2005",
+         "2006", "2007", "2008", "2009", "2010",
+         "2011", "2012", "2013", "2014", "2015"]
+
+# get multi annual averages
+def re(path,name):
+    data = xr.open_dataset(path)
+    d = weighted_temporal_mean(data,name)
+    d.name = name
+    return d
+
+df_tot = pd.DataFrame(columns = ["lat", "lon"])
+for name, var in zip(name_list, var_list):
+
+    # get annual averages
+    data = []
+    for y in years:
+        path = data_path + var + y + ".nc"
+        data.append(re(path,name))
+
+    # get average of all years
+    data_all_years = xr.concat(data,"time")
+    data_avg = data_all_years.mean("time")
+
+    # transform into dataframe
+    df = data_avg.to_dataframe().reset_index()
+    df[name] = df[name] * (10**6/86400)*12.87 # MJ m^-2 d^-1 into W m^-2 into mm/y
+    df_tot = pd.merge(df_tot, df, on=['lat', 'lon'], how='outer')
+
+data_path = "C:/Users/gnann/Documents/PYTHON/GHM_Comparison/"
+df_domains = pd.read_csv(data_path + "model_outputs/2b/aggregated/domains.csv", sep=',')
+df = pd.merge(df_tot, df_domains, on=['lat', 'lon'], how='outer')
+df = df.dropna()
+df_FLUXCOM = df
+print("Finished FLUXCOM.")
+"""
+
+# Cuthbert
+data_path = "C:/Users/gnann/Documents/PYTHON/Recharge/results/"
+df = pd.read_csv(data_path + "green-roofs_deep_drainage.csv")
+df_Cuthbert = df
+print("Finished Cuthbert.")
+
+# plot standard Budyko plot
+print("Several fluxes Budyko")
+stat = "median"
+fig = plt.figure(figsize=(6, 4), constrained_layout=True)
+axes = plt.axes()
+#im = axes.scatter(df_FLUXCOM["aridity_netrad_gswp3"], 1-df_FLUXCOM["LE"]/df_FLUXCOM["pr_gswp3"], s=1, c="#F7C188", alpha=0.25, lw=0)
+#im = axes.scatter(df_Caravan["aridity"], (df_Caravan["Q_mean"]/df_Caravan["p_mean"]), s=2.5, c="#a6cee3", alpha=0.25, lw=0)
+#im = axes.scatter(df_Caravan["aridity"], ((df_Caravan["BFI"]*df_Caravan["Q_mean"])/df_Caravan["p_mean"]), s=2.5, c="#1f78b4", alpha=0.25, lw=0)
+#im = axes.scatter(df_CAMELS["aridity"], (1-(1-df_Caravan["BFI"])*(df_Caravan["Q_mean"]/df_Caravan["p_mean"])), s=2.5, c="#947351", alpha=0.25, lw=0)
+#im = axes.scatter(df_Moeck["aridity_hpet"], (df_Moeck["Groundwater recharge [mm/y]"]/df_Moeck["tp"]), s=2.5, c="#b2df8a", alpha=0.25, lw=0)
+#im = axes.scatter(df_MacDonald["aridity_hpet"], (df_MacDonald["Recharge_mmpa"]/df_MacDonald["tp"]), s=2.5, c="#33a02c", alpha=0.25, lw=0)
+#plotting_fcts.plot_lines_group(df_FLUXCOM["aridity_netrad_gswp3"], df_FLUXCOM["LE"]/df_FLUXCOM["pr_gswp3"], "#F7C188", n=11, label='ET Fluxcom')
+plotting_fcts.plot_lines_group(df_ERA5["aridity_netrad"], df_ERA5["e"]/df_ERA5["tp"], "#CEA97C", n=11, label='ET ERA5', statistic=stat)
+plotting_fcts.plot_lines_group(df_ERA5["aridity_hpet"], df_ERA5["e"]/df_ERA5["tp"], "#CEA97C", n=11, label='ET ERA5', statistic=stat)
+plotting_fcts.plot_lines_group(df_Caravan["aridity_netrad"], (df_Caravan["runoff_ratio"]), "#a6cee3", n=11, label='Qtot', statistic=stat)
+#plotting_fcts.plot_lines_group(df_Caravan["aridity_netrad"], ((df_Caravan["BFI"]*df_Caravan["Q_mean"])/df_Caravan["p_mean"]), "#1f78b4", n=11, label='Qb')
+#plotting_fcts.plot_lines_group(df_Caravan["aridity_netrad"], (1-(1-df_Caravan["BFI"])*(df_Caravan["Q_mean"]/df_Caravan["p_mean"])), "#947351", n=11, label='P-Qf')
+plotting_fcts.plot_lines_group(df_Moeck["aridity_netrad"], df_Moeck["recharge_ratio"], "#b2df8a", n=11, label='GWR1', statistic=stat)
+plotting_fcts.plot_lines_group(df_MacDonald["aridity_netrad"], df_MacDonald["recharge_ratio"], "#33a02c", n=6, label='GWR2', statistic=stat)
+#plotting_fcts.plot_lines_group(df_Cuthbert["PET"]/df_Cuthbert["P"], df_Cuthbert["AET"]/df_Cuthbert["P"], "#c26900", n=11, label='ET/P')
+#plotting_fcts.plot_lines_group(df_Cuthbert["PET"]/df_Cuthbert["P"], df_Cuthbert["D(=P-AET)"]/df_Cuthbert["P"], "#A496CF", n=11, label='D/P')
+im = axes.plot(np.linspace(0.1,10,100), 1-Budyko_curve(np.linspace(0.1,10,100)), "--", c="black", alpha=0.75)
+im = axes.plot(np.linspace(0.1,10,100), 1-Budyko_curve(np.linspace(0.1,10,100)), "--", c="#a6cee3", alpha=0.75, label="Budyko")
+im = axes.plot(np.linspace(0.1,10,100), Berghuijs_recharge_curve(np.linspace(0.1,10,100)), "--", c="black", alpha=0.75)
+im = axes.plot(np.linspace(0.1,10,100), Berghuijs_recharge_curve(np.linspace(0.1,10,100)), "--", c="#b2df8a", alpha=0.75, label="Berghuijs")
+axes.set_xlabel("PET / P [-]")
+axes.set_ylabel("Flux / P [-]")
+axes.set_xlim([0.2, 5])
+axes.set_ylim([-0.1, 1.1])
+axes.legend(loc='center right', bbox_to_anchor=(1.35, 0.5))
+axes.set_xscale('log')
+plotting_fcts.plot_grid(axes)
+fig.savefig(figures_path + "Budyko_recharge_all_fluxes.png", dpi=600, bbox_inches='tight')
+plt.close()
+
+
+
+# plot standard Budyko plot
+print("Recharge Budyko")
+fig = plt.figure(figsize=(6, 4), constrained_layout=True)
+axes = plt.axes()
+im = axes.scatter(df_Moeck["aridity_netrad"], df_Moeck["recharge_ratio"], s=2.5, c="#b2df8a", alpha=0.25, lw=0)
+im = axes.scatter(df_MacDonald["aridity_netrad"], df_MacDonald["recharge_ratio"], s=2.5, c="#33a02c", alpha=0.25, lw=0)
+plotting_fcts.plot_lines_group(df_Moeck["aridity_netrad"], df_Moeck["recharge_ratio"], "#b2df8a", n=11, label='GWR1')
+plotting_fcts.plot_lines_group(df_MacDonald["aridity_netrad"], df_MacDonald["recharge_ratio"], "#33a02c", n=6, label='GWR2')
+plotting_fcts.plot_lines_group(df_Cuthbert["PET"]/df_Cuthbert["P"], df_Cuthbert["D(=P-AET)"]/df_Cuthbert["P"], "#A496CF", n=11, label='D/P')
+m = axes.plot(np.linspace(0.1,10,100), Berghuijs_recharge_curve(np.linspace(0.1,10,100)), "--", c="black", alpha=0.75)
+im = axes.plot(np.linspace(0.1,10,100), Berghuijs_recharge_curve(np.linspace(0.1,10,100)), "--", c="#b2df8a", alpha=0.75, label="Berghuijs")
+axes.set_xlabel("PET / P [-]")
+axes.set_ylabel("Flux / P [-]")
+axes.set_xlim([0.2, 5])
+axes.set_ylim([-0.1, 1.1])
+axes.legend(loc='center right', bbox_to_anchor=(1.35, 0.5))
+axes.set_xscale('log')
+plotting_fcts.plot_grid(axes)
+fig.savefig(figures_path + "Budyko_recharge.png", dpi=600, bbox_inches='tight')
+plt.close()
--- prepare_global_forcing_data.py
+++ prepare_global_forcing_data.py
@@ -11,6 +11,10 @@ import shapely.geometry as sgeom
 from brewer2mpl import brewer2mpl
 import matplotlib.ticker as mticker
 
+figures_path = "figures/"
+if not os.path.isdir(figures_path):
+    os.makedirs(figures_path)
+
 results_path = "results/"
 if not os.path.isdir(results_path):
     os.makedirs(results_path)
@@ -18,50 +22,68 @@ if not os.path.isdir(results_path):
 def aggregate_era5(data_path, var):
     file_path = data_path + var + "/data.nc"
     ds = xr.open_dataset(file_path)
-    ds = ds.sel(time=slice(dt(1975, 1, 1), dt(2004, 12, 31)))
+    ds = ds.sel(time=slice(dt(1981, 1, 1), dt(2020, 12, 31)))
     #ds = ds.resample(time="1Y").sum(min_count=1)
     ds = ds.mean(dim='time')
     ds = ds.assign_coords(longitude=(((ds.longitude + 180) % 360) - 180)) # shift coordinates
     ds = ds.sortby(ds.longitude)
     ds["longitude"] = np.round(ds["longitude"], 1)
     ds["latitude"] = np.round(ds["latitude"], 1)
-    df = ds.to_dataframe().reset_index().dropna()
     ds.to_netcdf(results_path + "ERA5_" + var + ".nc4")
+    df = ds.to_dataframe().reset_index().dropna()
     df.to_csv(results_path + "ERA5_" + var + ".csv", index=False)
     del ds
     del df
     print("finished aggregation", var)
 
-"""
-file_path = "D:/Data/ERA5/p/data.nc"
-ds = xr.open_dataset(file_path)
-#ds = ds.sel(time=slice(dt(1975, 1, 1), dt(2004, 12, 31)))
-ds = ds.mean(dim='time')
-pr = ds['tp']
-#(Optional) convert longitude from (0-360) to (-180 to 180) (if required)
-pr.coords['longitude'] = (pr.coords['longitude'] + 180) % 360 - 180
-pr = pr.sortby(pr.longitude)
-#Define lat/long
-pr = pr.rio.set_spatial_dims('longitude', 'latitude')
-#Check for the CRS
-pr.rio.crs
-pr.rio.set_crs("epsg:4326")
-pr.rio.to_raster(results_path + r"pr_GeoTIFF.tif")
-"""
+def save_as_geotif(data_path, var):
+    file_path = data_path + var + "/data.nc"
+    ds = xr.open_dataset(file_path)
+    ds = ds.sel(time=slice(dt(1981, 1, 1), dt(2020, 12, 31)))
+    ds = ds.mean(dim='time')
+    ds_var = ds['tp']
+    ds_var.coords['longitude'] = (ds_var.coords['longitude'] + 180) % 360 - 180
+    ds_var = ds_var.sortby(ds_var.longitude)
+    ds_var = ds_var.rio.set_spatial_dims('longitude', 'latitude')
+    ds_var.rio.set_crs("epsg:4326")
+    ds_var.rio.to_raster(results_path + "ERA5_" + var + ".tif")
+    del ds
+    del ds_var
+    print("finished geotif", var)
 
-# aggregate datasets
 data_path = "D:/Data/ERA5/"
 var_list = ["p", "pet", "aet", "rs", "rl", "lh"]
-for var in var_list:
-    aggregate_era5(data_path,var)
 
-# todo: load netcdf and mege them
-# load and merge aggregated datasets
+# aggregate datasets
+#for var in var_list:
+#    aggregate_era5(data_path,var)
+
+# load and merge aggregated nc4 datasets
+ds = xr.open_dataset(results_path + "hPET.nc4")
+for var in var_list:
+    ds_tmp = xr.open_dataset(results_path + "ERA5_" + var + ".nc4")
+    ds = xr.combine_by_coords([ds, ds_tmp])
+
+ds["tp"] = ds["tp"]*1000*365 # convert to mm/yr
+ds["e"] = ds["e"]*-1000*365 # convert to mm/yr
+ds["pev"] = ds["pev"]*-1000*365 # convert to mm/yr
+ds["ssr"] = ds["ssr"]/86400 # convert to W/m2
+ds["str"] = ds["str"]/86400 # convert to W/m2
+ds["slhf"] = ds["slhf"]/-86400 # convert to W/m2
+ds["aridity_era5"] = ds["pev"]/ds["tp"]
+ds["aridity_hpet"] = ds["pet"]/ds["tp"]
+ds["netrad"] = (ds["ssr"] + ds ["str"])*12.87 # convert to mm/yr
+ds["aridity_netrad"] = ds["netrad"]/ds["tp"]
+ds["aet_lh"] = ds["slhf"]*12.87 # convert to mm/yr
+ds.to_netcdf(results_path + "ERA5_aggregated.nc4")
+
+# load and merge aggregated csv datasets
 df = pd.read_csv(results_path + "hPET.csv")
 for var in var_list:
     df_tmp = pd.read_csv(results_path + "ERA5_" + var + ".csv")
     df = pd.merge(df, df_tmp, on=['latitude', 'longitude'], how='outer')
 
+# convert units
 df["tp"] = df["tp"]*1000*365 # convert to mm/yr
 df["e"] = df["e"]*-1000*365 # convert to mm/yr
 df["pev"] = df["pev"]*-1000*365 # convert to mm/yr
@@ -73,14 +95,9 @@ df["aridity_hpet"] = df["pet"]/df["tp"]
 df["netrad"] = (df["ssr"] + df ["str"])*12.87 # convert to mm/yr
 df["aridity_netrad"] = df["netrad"]/df["tp"]
 df["aet_lh"] = df["slhf"]*12.87 # convert to mm/yr
-#df.to_csv(results_path + "aridity.csv", index=False)
-
-ds = xr.Dataset.from_dataframe(df)
-ds = ds.set_coords(['latitude', 'longitude'])
-ds.to_netcdf(results_path + "aridity.nc4")
+df.to_csv(results_path + "ERA5_aggregated.csv", index=False)
 
 # aggregate hPET dataset
-data_path = "D:/Data/hPET/"
 def aggregate_hpet(data_path):
     ds_list = []
     for file_id in range(1981,2021):
@@ -93,11 +110,14 @@ def aggregate_hpet(data_path):
     ds_pet = ds_pet.where(np.isfinite(ds_pet), np.nan)
     ds_pet["longitude"] = np.round(ds_pet["longitude"], 1)
     ds_pet["latitude"] = np.round(ds_pet["latitude"], 1)
-    df_pet = ds_pet.to_dataframe().reset_index().dropna()
     ds_pet.to_netcdf(results_path + "hPET.nc4")
+    df_pet = ds_pet.to_dataframe().reset_index().dropna()
     df_pet.to_csv(results_path + "hPET.csv", index=False)
+    del ds_pet
+    del df_pet
     print("finished hpet")
 
+data_path = "D:/Data/hPET/"
 #aggregate_hpet(data_path)
 
 # plot results
@@ -108,18 +128,44 @@ fig = plt.figure()
 ax = plt.axes(projection=ccrs.Robinson())
 ax.set_global()
 customnorm = ml_colors.BoundaryNorm(boundaries=np.linspace(0,2,11), ncolors=256)
-sc = ax.scatter(df["longitude"], df["latitude"], c=df["aridity"], cmap=c, marker='s', s=.35, edgecolors='none',
+sc = ax.scatter(df["longitude"], df["latitude"], c=df["aridity_netrad"], cmap=c, marker='s', s=.35, edgecolors='none',
                 norm=customnorm, transform=ccrs.PlateCarree())
 # ax.coastlines(linewidth=0.5)
 box = sgeom.box(minx=180, maxx=-180, miny=90, maxy=-60)
 x0, y0, x1, y1 = box.bounds
 ax.set_extent([x0, x1, y0, y1], ccrs.PlateCarree())
 cbar = plt.colorbar(sc, orientation='horizontal', pad=0.01, shrink=.5, extend='max')
-cbar.set_label("Aridity")
+cbar.set_label("Aridity NetRad")
 # cbar.set_ticks([-100,-50,-10,-1,0,1,10,50,100])
 cbar.ax.tick_params(labelsize=12)
 plt.gca().outline_patch.set_visible(False)
 gl = ax.gridlines(draw_labels=False, linewidth=0.5, color='grey', alpha=0.75, linestyle='-')
 gl.xlocator = mticker.FixedLocator([-120, -60, 0, 60, 120])
 gl.ylocator = mticker.FixedLocator([-60, -30, 0, 30, 60])
-plt.show()
+fig.savefig(figures_path + "aridity_netrad_map.png", dpi=600, bbox_inches='tight')
+plt.close()
+
+
+o = brewer2mpl.get_map("RdBu", "Diverging", 9, reverse=True) # prepare colour map
+c = o.mpl_colormap
+plt.rcParams['axes.linewidth'] = 0.1
+fig = plt.figure()
+ax = plt.axes(projection=ccrs.Robinson())
+ax.set_global()
+customnorm = ml_colors.BoundaryNorm(boundaries=np.linspace(0,2,11), ncolors=256)
+sc = ax.scatter(df["longitude"], df["latitude"], c=df["aridity_hpet"], cmap=c, marker='s', s=.35, edgecolors='none',
+                norm=customnorm, transform=ccrs.PlateCarree())
+# ax.coastlines(linewidth=0.5)
+box = sgeom.box(minx=180, maxx=-180, miny=90, maxy=-60)
+x0, y0, x1, y1 = box.bounds
+ax.set_extent([x0, x1, y0, y1], ccrs.PlateCarree())
+cbar = plt.colorbar(sc, orientation='horizontal', pad=0.01, shrink=.5, extend='max')
+cbar.set_label("Aridity hPET")
+# cbar.set_ticks([-100,-50,-10,-1,0,1,10,50,100])
+cbar.ax.tick_params(labelsize=12)
+plt.gca().outline_patch.set_visible(False)
+gl = ax.gridlines(draw_labels=False, linewidth=0.5, color='grey', alpha=0.75, linestyle='-')
+gl.xlocator = mticker.FixedLocator([-120, -60, 0, 60, 120])
+gl.ylocator = mticker.FixedLocator([-60, -30, 0, 30, 60])
+fig.savefig(figures_path + "aridity_hpet_map.png", dpi=600, bbox_inches='tight')
+plt.close()
